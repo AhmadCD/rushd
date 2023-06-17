cd74dd8e03f9d0294a49fe7f3b8049e040d44798
AhmadCD <89347381+AhmadCD@users.noreply.github.com>
2023-06-17 19:17:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,109 @@
+import tensorflow as tf
+import numpy as np
+
+# Sample Data
+data_X = np.array([
+    [1, 1, 1, 1],
+    [1, 1, 1, 0],
+    [1, 1, 0, 1],
+    [1, 1, 0, 0],
+    [1, 0, 1, 1],
+    [1, 0, 1, 0],
+    [1, 0, 0, 1],
+    [1, 0, 0, 0],
+    [0, 0, 0, 1],
+    [0, 1, 1, 1],
+    [0, 1, 1, 0],
+    [0, 1, 0, 1],
+    [0, 1, 0, 0],
+    [0, 0, 1, 1],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+    [0, 0, 0, 0],
+])
+data_y = np.array([1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0])
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(32, input_shape=(4,), activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(data_X, data_y, epochs=150)
+
+#تعريف floats 
+cap_rate = float
+operating_expense_ratio = float
+break_even_ratio = float
+COC = float
+
+total_rental_income = float(input("Enter total rental income: "))
+operating_expenses = float(input("Enter operating expenses: "))
+NOI = total_rental_income - operating_expenses
+current_market_value = float(input("Enter current market value: "))
+cap_rate = NOI/current_market_value
+market_value = NOI/cap_rate
+investors_cap_rate = float(input("Enter Expected rate of return: "))
+economic_value = NOI/investors_cap_rate
+other_income = float(input("Enter any other income source if available: "))
+GOI = total_rental_income + other_income
+operating_expense_ratio = operating_expenses/GOI
+net_income_multiplier = market_value/NOI
+debt_service = float(input("Enter debt services: "))
+break_even_ratio = (operating_expenses + debt_service)/GOI
+gross_rental_yeild = total_rental_income/operating_expenses
+total_cash_invested = float(input("Enter Total Cash Invested: "))
+annual_cash_flow = total_rental_income + other_income - operating_expenses - debt_service
+COC = annual_cash_flow/total_cash_invested
+
+if cap_rate >= 0.06:
+  cap_rate = 1
+else:
+  cap_rate = 0
+
+if operating_expense_ratio <= 1:
+  operating_expense_ratio = 1
+else:
+  operating_expense_ratio = 0
+
+if break_even_ratio <= 1:
+  break_even_ratio = 1
+else:
+  break_even_ratio = 0
+
+if COC >= 0:
+  COC = 1
+else:
+  COC = 0
+
+input_data = np.array([[cap_rate, operating_expense_ratio, break_even_ratio, COC]])
+threshold = 0.1
+
+prediction = model.predict(input_data)
+threshold = 0.1
+
+readable_list = []
+
+for i in prediction:
+  if i[0] >= 0.1:
+    readable_list.append("System Output: Proceed with the investment")
+  else:
+    readable_list.append("System Output: DO NOT Proceed with the investment")
+
+print("feasibility KPI'S")
+print("Market Value : ", market_value)
+print("Econmic Value : ", economic_value)
+print("Break even ratio : ", break_even_ratio)
+print("Cap Rate : ", cap_rate)
+print("Net Income Multiplier : ", net_income_multiplier)
+
+print("Operating KPI's")
+print("Net Operating Income : ", NOI)
+print("Gross Operating Income : ", GOI)
+print("Opreating Expence ratio : ", operating_expense_ratio)
+print("Gross Rental Yeild : ", gross_rental_yeild)
+
+print("Cash Flow KPI's")
+print("Annual Cash Flow : ", annual_cash_flow)
+print("Cash on Cash return : ", COC)
+
+print(readable_list[0])
